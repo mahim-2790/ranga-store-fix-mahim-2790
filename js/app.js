da5c65af3628f214c405c133b1313def1e7d5de9
Mohi Uddin Mahim <mohi35-2790@diu.edu.bd>
2021-09-13 10:50:02 +0600

    bug solved

--- js/app.js
+++ js/app.js
@@ -25,6 +25,8 @@ const showProducts = (products) => {
         <h3>${product.title}</h3>
         <p>Category: ${product.category}</p>
         <h2>Price: $ ${product.price}</h2>
+        // <h2>Price: $ ${product.price}</h2>
+        // <h2>Price: $ ${product.price}</h2>                 
         <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
         <button id="details-btn" class="btn btn-danger">Details</button>
       </div>
@@ -36,14 +38,14 @@ let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
-
   updateTaxAndCharge();
+  updateTotal();
   document.getElementById("total-Products").innerText = count;
 };
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -52,12 +54,14 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  value = parseFloat(value).toFixed(2);
+  // console.log(parseFloat(value));
+  document.getElementById(id).innerText = value;
 };
 
 // update delivery charge and total Tax
@@ -82,5 +86,10 @@ const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
+updateTotal();
+
+const fetchRating = () => {
+
+}
